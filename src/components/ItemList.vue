1bd4123f90d6c3f824feca8a6f4e6de286379633
ponjuice2020 <ponjuice2021@gmail.com>
2023-06-10 15:51:05 +0900

    make itemList

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -14,31 +14,38 @@ const newItemName = ref('')
 const newItemPrice = ref(0)
 
 const addItem = () => {
-  items.value.push({ name: newItemName.value, price: newItemPrice.value })
+  if (newItemName.value != "") {  
+    items.value.push({ name: newItemName.value, price: newItemPrice.value });
+    newItemName.value = "";
+  }
 }
 </script>
-
 <template>
-  <div>
-    <div>ItemList</div>
-    <ul>
-      <li v-for="item in items" :key="item.name">
-        <div>名前: {{ item.name }}</div>
-        <div>{{ item.price }} 円</div>
-      </li>
-    </ul>
     <div>
-      <label>
-        名前
-        <input v-model="newItemName" type="text" />
-      </label>
-      <label>
-        価格
-        <input v-model="newItemPrice" type="number" />
-      </label>
-      <button @click="addItem">追加</button>
+      <div>ItemList</div>
+      <ul>
+        <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
+          <div>名前: {{ item.name }}</div>
+          <div>{{ item.price }} 円</div>
+          <div v-if="item.price >= 10000">高額商品</div>
+        </li>
+      </ul>
+      <div>
+        <label>
+          名前
+          <input v-model="newItemName" type="text" />
+        </label>
+        <label>
+          価格
+          <input v-model="newItemPrice" type="number" />
+        </label>
+        <button @click="addItem">add</button>
+      </div>
     </div>
-  </div>
-</template>
-
-<style></style>
+  </template>
+  
+  <style>
+  .over500 {
+    color: red;
+  }
+  </style>
